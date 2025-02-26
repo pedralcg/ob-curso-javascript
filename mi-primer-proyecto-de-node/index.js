180e3d1425ca6d4a77367c8d4b657d896bfb61a5
Pedro Alcoba <pedralcg@gmail.com>
2025-02-26 14:20:24 +0100

    Creo mi 1er proyecto de Node

---
+++ mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,9 @@
+// Este es el mensaje de bienvenida
+console.log('Esta es la puerta de entrada al proyecto');
+
+// Esta es la partes intermedia de nuestro proyecto
+// En esta parte realizamos los chequeos necesarios
+// Por último vamos a la última línea de saludo
+
+// Este es el mensaje que se envía para despedir
+console.log("Adiós");
